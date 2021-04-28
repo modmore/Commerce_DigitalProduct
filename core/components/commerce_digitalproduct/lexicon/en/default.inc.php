06bd72ad76b1bcb7d41f269ea9e696650296cafb
Mark Hamstra <hello@markhamstra.com>
2021-04-28 23:53:47 +0200

    Lexicon cleanup and cleaner filelinks interface

--- core/components/commerce_digitalproduct/lexicon/en/default.inc.php
+++ core/components/commerce_digitalproduct/lexicon/en/default.inc.php
@@ -16,30 +16,22 @@ $_lang['commerce_digitalproduct.product_tab'] = 'Digital';
 
 // Shipment fields
 $_lang['commerce_digitalproduct.dl_limit'] = 'Download Limit';
-$_lang['commerce_digitalproduct.dl_limit_desc'] = 'Maximum amount of times the user can download the files / resources attached to this product.';
+$_lang['commerce_digitalproduct.dl_limit_desc'] = 'Maximum number of times the user can download the files or view the resources attached to this product. Each file or resource has its own counter.';
 $_lang['commerce_digitalproduct.dl_expiry'] = 'Expiration Time';
-$_lang['commerce_digitalproduct.dl_expiry_desc'] = 'Time the user has to download the files / resources attached to this product.';
+$_lang['commerce_digitalproduct.dl_expiry_desc'] = 'Time the user has to download the files or view resources attached to this product. Other expiration options may be added to the Digital Product system settings.';
 $_lang['commerce_digitalproduct.dl_method'] = 'Download Method';
-$_lang['commerce_digitalproduct.dl_method_desc'] = 'Download method used for the file.';
+$_lang['commerce_digitalproduct.dl_method_desc'] = 'Download method used for the file. With the "redirect" method the user is redirected to the file or resource, and with the "force" method the secret URL will trigger a download without exposing the URL.';
 $_lang['commerce_digitalproduct.dl_expiry_format'] = 'Invalid time format.';
-$_lang['commerce_digitalproduct.user_group'] = 'User Group';
+$_lang['commerce_digitalproduct.user_group'] = 'User Group Access';
 $_lang['commerce_digitalproduct.user_group_desc'] = 'The user group to grant customers access to when they\'ve bought this product.';
 
 // Product fields
 $_lang['commerce_digitalproduct.resources'] = 'Resources';
-$_lang['commerce_digitalproduct.resources_desc'] = 'Resources that the customer can buy. For use with user group.';
+$_lang['commerce_digitalproduct.resources_desc'] = 'Resources that the customer can buy access to.';
 $_lang['commerce_digitalproduct.files'] = 'Files';
-$_lang['commerce_digitalproduct.files_desc'] = 'Full links to the file.';
+$_lang['commerce_digitalproduct.files_desc'] = 'Files that may be sold as a download. Each file needs to be specified by its full URL and at this time needs to be publicly downloadable. To add more files, save the product.';
 
-// Resource Link field fields
-$_lang['commerce_digitalproduct.existing_resources'] = 'Existing Digital Resource(s)';
-$_lang['commerce_digitalproduct.add_resource'] = 'Add a Digital Resource';
-$_lang['commerce_digitalproduct.resource'] = 'Resource';
 $_lang['commerce_digitalproduct.resource_display_name'] = 'Display Name (shows to customer)';
-
-// File Link field fields
-$_lang['commerce_digitalproduct.existing_files'] = 'Existing File URL(s)';
-$_lang['commerce_digitalproduct.add_file'] = 'Add a File (URL)';
 $_lang['commerce_digitalproduct.url'] = 'URL to File';
 
 // Frontend
