f5dd7f33712c15300619f6205cc261b8c81cf7e5
Mark Hamstra <hello@markhamstra.com>
2021-04-28 18:07:58 +0200

    Apparently the schema generation removes the comments on the map file.
    Not sure why (perhaps manually added or originally built with a custom template?), but don't want to keep that uncommitted as it would then just keep getting back

--- core/components/commerce_digitalproduct/model/commerce_digitalproduct/mysql/digitalproduct.map.inc.php
+++ core/components/commerce_digitalproduct/model/commerce_digitalproduct/mysql/digitalproduct.map.inc.php
@@ -1,15 +1,4 @@
 <?php
-/**
- * Digitalproduct for Commerce.
- *
- * Copyright 2019 by Tony Klapatch <tony@klapatch.net>
- *
- * This file is meant to be used with Commerce by modmore. A valid Commerce license is required.
- *
- * @package commerce_digitalproduct
- * @license See core/components/commerce_digitalproduct/docs/license.txt
- */
-
 $xpdo_meta_map['Digitalproduct']= array (
   'package' => 'commerce_digitalproduct',
   'version' => '1.1',
